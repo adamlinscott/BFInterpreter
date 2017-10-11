0e53ac2a7a2cb9738add631830fd4337f4e97239
Adam Linscott <adam.linscott@probablyrational.com>
2017-10-11 14:43:15 +0100

    Starting point for Interpreter
    when running program from command line specify the file to be
    interpreted.
    
    the program will read all the data in the file to a string ready to be
    interpreted

---
+++ BFCompiler/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace BFCompiler
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string codeToCompile;
+
+			if(args.Length > 0)
+			{
+				codeToCompile = OpenFile(args[0]);
+			}
+			else
+			{
+				Console.WriteLine("File name expected as first argument.");
+				System.Environment.Exit(1);
+			}
+
+			//Continue with compilling string here
+
+
+		}
+
+		static string OpenFile(string filename)
+		{
+			try
+			{   // Open the text file using a stream reader.
+				using (StreamReader sr = new StreamReader(filename))
+				{
+					// Read the stream to a string, and write the string to the console.
+					String line = sr.ReadToEnd();
+					return line;
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("The file could not be read:");
+				Console.WriteLine(e.Message);
+				System.Environment.Exit(1);
+				return "";
+			}
+		}
+	}
+
+}
